96de0534f85a96556f44fb162df747aa9b7fd282
Belen Curcio <curciobelen@gmail.com>
2015-09-13 02:49:33 -0300

    la concha de tu madre callbacks

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 var urls = {
 	cad: 'https://api.bitcoinaverage.com/ticker/global/CAD/last',
 	usd: 'https://api.bitcoinaverage.com/ticker/global/USD/last',
-	cpl: 'https://api.bitcoinaverage.com/ticker/global/CLP/last'
+	clp: 'https://api.bitcoinaverage.com/ticker/global/CLP/last'
 };
 
 module.exports = urls;
--- middlewares.js
+++ middlewares.js
@@ -1,29 +1,54 @@
 var config = require('./config'),
-	request = require('request');
+	request = require('request'),
+	Q = require('q'),
+	_ = require('underscore');
 
 // turns group of strings into object
 exports.saveValuesToObject = function (callback) {
-	var result = {};
+	var result = [];
 
 	var coinsPaths = {
 		cad: config.cad,
 		usd: config.usd,
-		cpl: config.cpl
+		clp: config.clp
 	};
 
-
-	for (coin in coinsPaths) {
-		readValues(coinsPaths[coin], function (err, value) {
-			// este console.log de acá
-			console.log(coin);
-			if (err) return callback(err);
-
-			result[coin] = value;
+	var getValue = function(path, coin, i) {
+		var q = Q.defer();
+		readValues(path, function (err, value) {
+			var res = {};
+			res[coin] = value;
+			q.resolve(res);
 		});
+		return q.promise;
 	};
 
-	return callback(null, result);
+	var getAllValues = function() {
+		var q = Q.defer(),
+		pathsLength = _.size(coinsPaths),
+		allRes = [],
+		i = 1;
+		
+		for (var coin in coinsPaths) {
+			path = coinsPaths[coin];
+			var result = [];
+			getValue(path, coin, i).then(function(res){
+				result.push(res);
+				if (i == pathsLength) {
+					// Cuando recorrio todo el objeto, resuelvo la promise
+					q.resolve(result);
+				}
+				i++;
+			});
+		}
+		return q.promise;
+	};
 
+	getAllValues().then(function(result){
+		return callback(null, result);
+	});
+	
+	
 }
 
 // reads content of an url and returns an string with result
