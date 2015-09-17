c2c618ed93ea6a1b582fa52bc0ce52b133df895c
keyserfaty <keserfaty@gmail.com>
2015-09-16 22:18:14 -0300

    spinner working

--- src/config.js
+++ src/config.js
@@ -1,17 +1,30 @@
 var bitApp = angular.module('bitApp', ['angularSpinner']);
 
-bitApp.controller('mainController', ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
-	// meter todo esto en un función y llamarla cada 15 seg
+bitApp.controller('mainController', ['$scope', '$http', '$interval', 'usSpinnerService', function ($scope, $http, $interval, usSpinnerService) {
 		
-		var prices = {};
-		var time = [];
-		var activeCoins = ["CAD", "USD", "CLP"];
+	var prices = {};
+	var time = [];
+	var activeCoins = ["CAD", "USD", "CLP"];
+
+	// Make POST call when site is loaded
+	$scope.init = function () {
+		$http.post('/saveNew')
+			.then(function (res) {
+				console.log('New entries created successfully');
+
+			}, function (err) {
+				console.log(err);
+		});
+	}
+
+	function getAll () {
+		// Starts spinner while view is getting updated
+		usSpinnerService.spin('spinner-1');
 
 		$http.get('/getAll')
 
 			.success(function (data) {
 
-				$interval(function () {
 					// Creates object of prices and currencies to display in view
 					activeCoins.forEach(function (coin) {
 						for (elem in data[0]) {
@@ -23,28 +36,23 @@ bitApp.controller('mainController', ['$scope', '$http', '$interval', function ($
 					// Saves time of last update in array to display in view
 					time.push(data[0]["time"]);
 					$scope.time = time[0];
-				
-				}, 1000);
 
-
-				// Makes a call to POST method every time the page is loaded
-
-				$http.post('/saveNew')
-					.then(function (res) {
-						console.log('New entries created successfuly');
-						// usSpinnerService.stop('spinner-1');
-
-					}, function (err) {
-						console.log(err);
-				});
-
-				// usSpinnerService.spin('spinner-1');
-				
+					// View is updated so spinner stops
+					usSpinnerService.stop('spinner-1');
 			})
 
 			.error(function (data) {
 				console.log('Error: ' + data);
-			});
+			});		
+	}
 
+	// Calls GET method for the first time
+	getAll();
+
+	// Makes a call to GET method every 15 seconds to bring updated data from db
+	$interval(function () {
+		console.log('Updates view');
+		getAll();
+	}, 15000);
 
 }]);
